9a94cabbea1a4b8cefb7d09b1299152fadfa1d56
anandhapandiyan1-a11y <anandhapandiyan1@gmail.com>
2026-09-06 10:31:12 +0530

    Remove dotenv and use streamlit secrets

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import io
 import os
 import time
-from dotenv import load_dotenv
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 
-load_dotenv()
 
 # Custom Core & Agent Modules
 from agents.business_agent import BusinessAgent
